07ab46b55ace7150f8ff88fb036ddd85f189f699
Andrew <example@gmail.com>
2020-03-25 19:44:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log(leftBorderWidth)
+
+var leftBorderWidth = 1;
+
+let second = 2;
+
+const pi = 3.14;
+
+let arr = ['plu.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+// alert ('Hello World!');
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+let answer = prompt("Are you 18?", "Yes");
+console.log(typeof(arr));
